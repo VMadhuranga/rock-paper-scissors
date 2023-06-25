a9872f633e791721880e2b7ba0eabaa43f6cb84f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-06-25 12:45:39 +0530

    Create function playRound
    This function has playerSelection and computerSelection
    parameter and check for parameter values according to:
    - rock && scissors
    - paper && rock
    - scissors && paper
    
    and output win or loose

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function getPlayerChoice() {
     let choice = prompt("Please select your choice from: Rock, Paper, Scissors ").toLocaleLowerCase();
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
