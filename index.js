b9aa7337c0f548d70be7e985373f594ef68a1ee6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-06-25 10:47:15 +0530

    Create function getPlayerChoice
    This function ask user to choose from
    - Rock
    - Paper
    - Scissors
    
    and return the value

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// define a function getComputerChoice
-// randomly output "Rock", "Paper", "Scissors"
-
 function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"];
     return choice[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let choice = prompt("Please select your choice from: Rock, Paper, Scissors ").toLocaleLowerCase();
+    return choice;
+}
