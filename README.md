afd11271936d5961791b1d4f45c0de0f2f034bbf
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-06-29 08:59:07 +0530

    Add user interface
    Create HTML structure and add CSS styling
    Remove getting user selection from prompt and add buttons to get
    user selection

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 This is The Odin Project's Rock Paper Scissors project assignment
+
+What's this program do:
+
+This is a small game that you can play against computer.
+
+Computer randomly generates choice from Rock, Paper Scissors
+and compare with user choice and gives score to winner.
+
+First one to get five point will be Winner.
+
+TODO:
+
+- Improve UI
+- Add play again button
--- index.js
+++ index.js
@@ -1,44 +1,64 @@
+let choices = document.querySelectorAll("button");
+
+let divTextContent = document.querySelector(".output > .text-content");
+let divPlayerScore = document.querySelector(".output > .player-score");
+let divComputerScore = document.querySelector(".output > .computer-score");
+let divWinner = document.querySelector(".output > .winner");
+
+let playerScore = 0;
+let computerScore = 0;
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", playRound);
+});
+
+divPlayerScore.textContent = `Player score: ${playerScore}`;
+divComputerScore.textContent = `Computer score: ${computerScore}`;
+
+function playRound(event) {
+    let playerSelection = event.target.className;
+    let computerSelection = getComputerChoice();
+    
+    if (playerSelection === computerSelection) {
+        divTextContent.textContent = "It's a tie";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        playerScore++;
+        divPlayerScore.textContent = `Player score: ${playerScore}`;
+
+        divTextContent.textContent = `You Scored! ${playerSelection[0].toUpperCase() + playerSelection.slice(1,)} \
+        beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1,)}`;
+    } else {
+        computerScore++;
+        divComputerScore.textContent = `Computer score: ${computerScore}`;
+
+        divTextContent.textContent = `Computer Scored! ${computerSelection[0].toUpperCase() + computerSelection.slice(1,)} \
+        beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1,)}`;
+    }
+
+    if (playerScore === 5 || computerScore === 5) {
+        declareWinner(playerScore, computerScore);
+    }
+}
+
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-    let choice = prompt("Please select your choice from: Rock, Paper, Scissors ").toLocaleLowerCase();
-    return choice;
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            console.log("It's a tie");
-        } else if (
-            (playerSelection === "rock" && computerSelection === "scissors") ||
-            (playerSelection === "paper" && computerSelection === "rock") ||
-            (playerSelection === "scissors" && computerSelection === "paper")
-        ) {
-            playerScore++;
-            console.log(`You Scored! ${playerSelection[0].toUpperCase() + playerSelection.slice(1,)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1,)}`);
-        } else {
-            computerScore++;
-            console.log(`Computer Scored! ${computerSelection[0].toUpperCase() + computerSelection.slice(1,)} beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1,)}`);
-        }
-    }
-        
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
-
+function declareWinner(playerScore, computerScore) {
     if (playerScore > computerScore) {
-        return "You Won! You beats Computer";
+        divWinner.textContent = "You won";
+        choices.forEach((choice) => {
+            choice.removeEventListener("click", playRound);
+        });
     } else {
-        return "You Lose! Computer beats you";
+        divWinner.textContent = "You loose!";
+        choices.forEach((choice) => {
+            choice.removeEventListener("click", playRound);
+        });
     }
 }
-
-console.log(game());
